49a07d87b1fb0b83c1f08bc222a122d5b5b41063
Clive <cluivee@gmail.com>
2022-06-05 14:30:07 +0100

    Adding new Calendar Component

--- src/MyCalendar.js
+++ src/MyCalendar.js
@@ -1,50 +1,119 @@
-import React, { useState } from "react";
-import { addMonths, subMonths, format } from 'date-fns'
+import React, {Component, useState} from 'react'
 import "./MyCalendar.css";
+
+import {
+  addMonths,
+  subMonths,
+  format,
+  startOfWeek,
+  addDays,
+  startOfMonth,
+  endOfMonth,
+  endOfWeek,
+  isSameMonth,
+  isSameDay,
+  parse,
+} from "date-fns";
 
 const MyCalendar = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState(new Date());
-
-  const nextMonth = () => {
-      setCurrentDate(addMonths(currentDate, 1));
-  }
-
-  const prevMonth = () => {
-      setCurrentDate(subMonths(currentDate, 1));
-  }
-
   const header = () => {
-    const dateFormat = "mmmm yyyy";
-
-    return (
-      <div className="header row flex-middle">
+  const dateFormat = "MMMM yyyy";
+  return (
+     <div className="header row flex-middle">
         <div className="column col-start">
-          <div className="icon" onClick={prevMonth}>
-            chevron_left
-          </div>
+           <div className="icon" onClick={prevMonth}>
+              chevron_left
+           </div>
         </div>
         <div className="column col-center">
-          <span>{format(currentDate, dateFormat)}</span>
+           <span>{format(currentDate, dateFormat)}</span>
         </div>
         <div className="column col-end">
-          <div className="icon" onClick={nextMonth}>
-            chevron_right
-          </div>
+           <div className="icon" onClick={nextMonth}>
+              chevron_right
+           </div>
         </div>
-      </div>
+     </div>
+     );
+  };
+  const days = () => {
+  const dateFormat = "iii";
+  const days = [];
+  let startDate = startOfWeek(currentDate);
+  for (let i = 0; i < 7; i++) {
+        days.push(
+           <div className="column col-center" key={i}>
+           {format(addDays(startDate, i), dateFormat)}
+           </div>
+        );
+     }
+     return <div className="days row">{days}</div>;
+  };
+  const cells = () => {
+  const monthStart = startOfMonth(currentDate);
+  const monthEnd = endOfMonth(monthStart);
+  const startDate = startOfWeek(monthStart);
+  const endDate = endOfWeek(monthEnd);
+  const dateFormat = "dd";
+  const rows = [];
+  let days = [];
+  let day = startDate;
+  let formattedDate = "";
+  while (day <= endDate) {
+     for (let i = 0; i < 7; i++) {
+     formattedDate = format(day, dateFormat);
+     const cloneDay = day;
+  days.push(
+        <div 
+         className={`column cell ${!isSameMonth(day, monthStart)
+         ? "disabled" : isSameDay(day, selectedDate) 
+         ? "selected" : "" }`} 
+         key={day} 
+         onClick={() => onDateClick(parse(cloneDay))}
+         > 
+         <span className="number">{formattedDate}</span>
+         <span className="bg">{formattedDate}</span>
+       </div>
+       );
+     day = addDays(day, 1);
+    }
+  rows.push(
+        <div className="row" key={day}> {days} </div>
+      );
+     days = [];
+   }
+   return <div className="body">{rows}</div>;
+  }
+  const nextMonth = () => {
+     setCurrentDate(addMonths(currentDate, 1));
+  };
+  const prevMonth = () => {
+     setCurrentDate(subMonths(currentDate, 1));
+  };
+  const onDateClick = day => {
+  setSelectedDate(day);
+  }
+  return (
+     <div className="calendar">
+        <div>{header()}</div>
+        <div>{days()}</div>
+        <div>{cells()}</div>
+     </div>
     );
   };
 
-  return (
-    <div className="myCalendar">
-      <div> {header()}</div>
-      {/* <div> {daysOfWeek()}</div>
-      <div> {cells()}</div> */}
-      <h1>Calendar</h1>
-      hello, this is the calendar component
-    </div>
-  );
-};
 
-export default MyCalendar;
+
+class App extends Component {
+    render() {
+        return (
+        <div>
+        <MyCalendar />
+        </div>
+        )
+    }
+}
+
+export default App
